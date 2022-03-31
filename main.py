985628b9c11bd839429d5eb6402e12e3227433cb
Scott Im <scottim@berkeley.edu>
2022-03-31 13:45:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from twilio.rest import Client
 
 load_dotenv()
 
-STOCK = "TSLA"
-COMPANY_NAME = "Tesla Inc"
+STOCK = "AAPL"
+COMPANY_NAME = "Apple Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
